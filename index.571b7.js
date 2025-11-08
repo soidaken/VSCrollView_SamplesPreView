65a064173d866dcdb68dad914e286267b6bdb3b7
soida <adioskensunny@gmail.com>
2025-11-08 23:42:54 +0800

    添加子项点击展开案例2

--- index.571b7.js
+++ index.571b7.js
@@ -1,4 +1,4 @@
-System.register(["./application.a36e4.js"], function (_export, _context) {
+System.register(["./application.fab81.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
