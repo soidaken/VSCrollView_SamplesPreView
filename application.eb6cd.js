7c8b49337fee241f0e2a160279117d4bce15db71
soida <adioskensunny@gmail.com>
2025-11-22 20:10:29 +0800

    增加列表的头部间距和尾部间距

--- application.eb6cd.js
+++ application.eb6cd.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.927e7.json';
+          this.settingsPath = 'src/settings.73557.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
